4250cd7b829b964d7bb231109a98539aa09e1649
Benedict <b.kupetz@fz-juelich.de>
2024-10-22 09:56:03 +0200

    generate grid from words

---
+++ src/generator/grid.py
@@ -0,0 +1,56 @@
+def generate(riddle, solution: str):
+    if len(riddle) != len(solution):
+        raise ValueError("Die Anzahl Rätselteile muss die selbe länge haben wie die Lösung")
+
+    x, y = 0, len(solution)
+    solution_idx = 0
+    prev_max, next_max = 0, 0
+
+    word_pos = [0 for _ in range(len(riddle))]
+
+    for i, part in enumerate(riddle):
+        idx = part[1].find(solution[i])
+        if idx == -1:
+            raise ValueError(f"Der Buchstabe {solution[i]} ist nicht im Rätselteil {part[1]} enthalten")
+        temp_prev = len(part[0]) + idx
+        temp_next = len("".join(part)) - temp_prev - 1
+
+        if i != 0:
+            if temp_prev > prev_max:
+                for j in range(i):
+                    word_pos[j] += temp_prev - prev_max
+            if temp_prev < prev_max:
+                word_pos[i] = prev_max - temp_prev
+
+        if temp_prev > prev_max:
+            prev_max = temp_prev
+            solution_idx = temp_prev
+
+        prev_max = max(prev_max, temp_prev)
+        next_max = max(next_max, temp_next)
+
+    x = prev_max + next_max + 1
+
+    grid = []
+    grid_solution = []
+    for i, r in enumerate(riddle):
+        out = " " * word_pos[i] + r[0] + " " * len(r[1]) + r[2]
+        grid.append([c for c in out])
+
+        out_solution = " " * word_pos[i] + "".join(r)
+        grid_solution.append([c for c in out_solution])
+
+
+    print(grid)
+
+
+
+def main():
+    riddle = [["vogel", "schreck", "schraube"], ["haar", "gummi", "baer", ], ["laden", "schluss", "licht"]]
+    solution = "eis"
+
+    generate(riddle, solution)
+
+
+if __name__ == "__main__":
+    main()
