2db6c11d385a64610ee45d1da11c873a8711e270
Benedict <b.kupetz@fz-juelich.de>
2024-10-22 09:56:23 +0200

    where the game is run

---
+++ src/cli/game.py
@@ -0,0 +1,7 @@
+import curses
+
+
+def run(stdscr: curses.window):
+    riddle = [["vogel", "schreck", "schraube"], ["haar", "gummi", "baer", ], ["laden", "schluss", "licht"]]
+    solution = "eis"
+
